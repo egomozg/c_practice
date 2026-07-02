6118253034f457eff7c1709714cbd1c2bdfdcffe
Egor Savchenko <egor.chocho@gmail.com>
2026-07-02 19:06:03 +0300

    update problem al

--- structures/problem_al.c
+++ structures/problem_al.c
@@ -1,5 +1,5 @@
 //
-// not finished yet, not tested yet
+// not finished yet
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,59 +12,72 @@ struct node_t {
 #endif
 
 struct node_t *read_list(FILE *inp);
-struct node_t *create_list();
 void delete_list(struct node_t *top);
-int emplace(struct node_t *node, const int data);
+void print_list(const struct node_t *top);
 
-struct node_t *create_list() {
-  struct node_t *top = (struct node_t *)calloc(1, sizeof(struct node_t));
-  top->next = (struct node_t *)calloc(1, sizeof(struct node_t));
-  return top;
-}
+int main(void) {
+  FILE *f;
+  int count = 0;
 
-int emplace(struct node_t *top, const int data) {
-  struct node_t *current = top;
-  while (current->next != NULL)
-    current->next = current->next->next;
-  current->next->data = data;
-  current->next->next = (struct node_t *)calloc(1, sizeof(struct node_t));
-  if (current->next->next == NULL) return 0; //failure
-  return 1; //success
+  f = fopen("test.txt", "r");
+  if (!f) {
+    perror("Error opening file");
+    return 1;
+  }
+  struct node_t *al = read_list(f);
+  print_list(al);
+  delete_list(al);
+
+  return 0;
 }
 
 struct node_t *read_list(FILE *inp) {
-  struct node_t *top_even = create_list();
-  struct node_t *top_odd = create_list();
-  struct node_t *iterable = top_even;
-  if (!top_even || !top_odd) {
-    return NULL;
+  struct node_t *top_even = NULL, *top_odd = NULL, *node;
+  for (;;) {
+    int res, n = 0;
+    res = fscanf(inp, "%d", &n);
+    if (res == EOF)
+      break;
+    if (res != 1) {
+      continue;
+    }
+    node = calloc(1, sizeof(struct node_t))
+    if (n % 2 == 0) {
+        node->next = top_even;
+        top_even = node;
+    }
+    else {
+        node->next = top_odd;
+        top_odd = node;
+    }
   }
-	for (;;) {
-		int res, n = 0;
-		res = fscanf(inp, "%d", &n);
-		if (res == EOF)
-			break;
-		if (res != 1) {
-			fprintf(stderr, "File have broken format\n");
-			abort();
-		}
-    if (n % 2 == 0)
-      emplace(top_even, n);
-    else
-      emplace(top_odd, n);
-	}
-  while (iterable != NULL) {
-    iterable = iterable->next;
+  while (node->next != NULL) {
+    node = node->next;
   }
-  iterable = top_odd;
   return top_even;
 }
 
 void delete_list(struct node_t *top) {
+  if (top == NULL) {
+    printf("tried to delete nullptr\n");
+    abort();
+  }
   struct node_t *next = top;
   while (next != NULL) {
     next = next->next;
     free(top);
     top = next;
   }
+}
+
+void print_list(const struct node_t *top) {
+    const struct node_t *i = top;
+    if (i == NULL) {
+    return;
+  }
+  while (i != NULL) {
+    printf("%d ", i->data);
+    i = i->next;
+  }
+  printf("\n");
 }
